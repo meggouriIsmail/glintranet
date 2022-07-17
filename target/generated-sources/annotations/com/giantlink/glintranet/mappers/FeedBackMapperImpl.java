61fb169729440b27ba651712ddfd4d2ce260f9e8
ismail meggouri <i.meggouri@giantlink.ma>
2022-07-17 15:10:30 +0100

    doc

--- target/generated-sources/annotations/com/giantlink/glintranet/mappers/FeedBackMapperImpl.java
+++ target/generated-sources/annotations/com/giantlink/glintranet/mappers/FeedBackMapperImpl.java
@@ -25,13 +25,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-<<<<<<< HEAD
-    date = "2022-07-13T14:18:48+0100",
+    date = "2022-07-17T15:10:21+0100",
     comments = "version: 1.4.2.Final, compiler: Eclipse JDT (IDE) 1.4.50.v20210914-1429, environment: Java 17.0.2 (Eclipse Adoptium)"
-=======
-    date = "2022-07-16T12:28:51+0100",
-    comments = "version: 1.4.2.Final, compiler: Eclipse JDT (IDE) 1.4.0.v20210708-0430, environment: Java 17 (Eclipse Adoptium)"
->>>>>>> e0c5843f9a874028499c6accdb15ca66f5bd7dd4
 )
 @Component
 public class FeedBackMapperImpl implements FeedBackMapper {
